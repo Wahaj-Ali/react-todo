49ef73c6e35a050f4cd0d42dda0e422ebf0cdced
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-01-16 21:24:31 +0500

    Added functional components instead of class-based components
    in this commit
    - used functional components.
    - stored data in local storage.
    - get data from local storage and display it on UI.

--- src/classbased/components/TodoContainer.js
+++ src/classbased/components/TodoContainer.js
@@ -6,23 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: 2,
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+    todos: []
   };
 
   handleChange = id => {
@@ -60,6 +44,40 @@ class TodoContainer extends React.Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
+    })
+  };
+
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+  //   .then(response => response.json())
+  //   .then(data => this.setState({ todos: data }));
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  };
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  };
+
   render() {
     return (
       <div className="container">
@@ -70,6 +88,7 @@ class TodoContainer extends React.Component {
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
+          setUpdate={this.setUpdate}
         />
       </div>
     </div>
